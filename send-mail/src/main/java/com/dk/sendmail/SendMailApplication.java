20f7c03aba06702d321484c27ff996e428503967
A-caro <carolinanatampis@gmail.com>
2023-02-26 16:06:41 -0300

    adding configurations

---
+++ send-mail/src/main/java/com/dk/sendmail/SendMailApplication.java
@@ -0,0 +1,16 @@
+package com.dk.sendmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+
+public class SendMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendMailApplication.class, args);
+	}
+
+}
---
+++ send-mail/src/test/java/com/dk/sendmail/SendMailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dk.sendmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendMailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
