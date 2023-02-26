60dcdb2449c476ac15c0ce1c4d79b1317d285967
A-caro <carolinanatampis@gmail.com>
2023-02-26 17:25:33 -0300

    changes

--- send-mail/src/main/java/com/dk/sendmail/SendMailApplication.java
+++ send-mail/src/main/java/com/dk/sendmail/SendMailApplication.java
@@ -6,9 +6,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
-
 public class SendMailApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SendMailApplication.class, args);
 	}
